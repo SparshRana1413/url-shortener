f21264ef20e8ccf89c39a43d043bf876015d6623
Sparsh Rana <sparsh.rana1413@gmail.com>
2026-07-29 00:34:23 +0530

    added redis client

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import './config/db.js';
 import cors from 'cors';
+import redisClient from './config/redis.js';
 
 import mainRouter from './routes/index.js';
 
@@ -11,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', mainRouter);
+
+try{
+  await redisClient.connect();
+} catch (err){
+  console.error("could not connect to redis client");
+}
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(
